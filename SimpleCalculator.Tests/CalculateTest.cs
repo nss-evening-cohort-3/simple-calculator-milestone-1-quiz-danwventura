c1daeba5583396b6c4b309daa12e337acc7af104
Dan Ventura <danwventura@icloud.com>
2016-10-12 23:15:19 -0500

    test for calculate class

--- SimpleCalculator.Tests/CalculateTest.cs
+++ SimpleCalculator.Tests/CalculateTest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace SimpleCalculator.Tests
 {
-    class Class1
+    [TestClass]
+    public class CalculateTests
     {
+        [TestMethod]
+        public void EnsureCalculateTestIsNotNull()
+        {
+            Calculate calculate = new Calculate();
+            Assert.IsNotNull(calculate);
+        }
+
+
     }
 }
